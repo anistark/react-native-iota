fca399c90b1953fe3e1be45d5db176c4d1f2607e
Kumar Anirudha <mail@anirudha.dev>
2023-01-31 16:10:44 +0530

    added iota client

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Client } from '@iota/client';
+
+const client = new Client({
+    nodes: ['https://api.testnet.shimmer.network'],
+});
 
 export default function App() {
+  client.getInfo().then(console.log).catch(console.error)
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
